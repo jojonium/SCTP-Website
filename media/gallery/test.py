2b054cc05bb9b3373c0931a7b86c25e2eafd8477
Joseph Petitti <jojonium@autoreg-072019.dyn.wpi.edu>
2019-02-20 11:34:50 +0800

    changed names

--- media/gallery/test.py
+++ media/gallery/test.py
@@ -2,8 +2,8 @@ of = open("out.txt", "w")
 
 for i in range(4, 55):
     of.write("<div class=\"pic-box\">")
-    of.write("\n  <a href=\"media/gallery/" + str(i) + ".jpg\">")
-    of.write("\n    <img src=\"media/gallery/thumb." + str(i) + ".jpg\">")
+    of.write("\n  <a href=\"media/gallery/" + str(i).zfill(2) + ".jpg\">")
+    of.write("\n    <img src=\"media/gallery/th_" + str(i).zfill(2) + ".jpg\">")
     of.write("\n  </a>")
     of.write("\n")
     of.write("\n  <h4>Placeholder Text</h4>")
--- media/gallery/thumbnailify.sh
+++ media/gallery/thumbnailify.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-SIZE=250
+SIZE=250;
+i=1;
+
+rm th_*;
+
 for file in *.jpg; do
-   echo -n Converting ${file}...
-   convert -resize ${SIZE}x${SIZE}^ "$file" temp.png
-   convert -crop $(identify temp.png | awk -F'[ x]' -v SIZE=$SIZE '{ printf "%ux%u+%u+%u", SIZE, SIZE, ($3-SIZE)/2, ($4-SIZE)/2 }') temp.png "th_$file"
-   echo done
+	mv "$file" $(printf "%02d.jpg" $i);
+	i=$((i+1))
+done
+echo Done renaming
+
+for file in *.jpg; do
+	echo -n Converting ${file}...
+	convert -resize ${SIZE}x${SIZE}^ "$file" temp.png
+	convert -crop $(identify temp.png | awk -F'[ x]' -v SIZE=$SIZE '{ printf "%ux%u+%u+%u", SIZE, SIZE, ($3-SIZE)/2, ($4-SIZE)/2 }') temp.png "th_$file"
+	echo done
 done
 rm temp.png
